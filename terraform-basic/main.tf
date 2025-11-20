38940c9920a0d503da9b0955a4d69dad4753b881
Rupesh Chede <rupeshchede60@gmail.com>
2025-11-20 19:21:35 +0530

    Update main.tf

--- terraform-basic/main.tf
+++ terraform-basic/main.tf
@@ -10,7 +10,7 @@ resource "aws_subnet" "name" {
   vpc_id = aws_vpc.name.id
   cidr_block = "10.0.0.0/24"
   tags = {
-    Name = "Public-Subnet"
+    Name = "subnet-1"
   }
   
 }
@@ -19,7 +19,7 @@ resource "aws_subnet" "name2" {
   vpc_id = aws_vpc.name.id
   cidr_block = "10.0.1.0/24"
   tags = {
-    Name = "Private-subnet"
+    Name = "subnet-2"
   }
   
 }
